69c2bf76c5d73eef9dc1e96eb61d640861fd2de5
minstrike520 <123291118+minstrike520@users.noreply.github.com>
2023-09-22 23:11:54 +0000

    chore (tools): added 2 small functions

--- utilities/src/tools.py
+++ utilities/src/tools.py
@@ -4,3 +4,15 @@ def is_intstr(s: str):
         return True
     except ValueError:
         return False
+
+def spaces(amount):
+    s = ""
+    for i in range(amount):
+        s += " "
+    return s
+
+def combine(li):
+    s = ""
+    for item in li:
+        s += item+" "
+    return s
