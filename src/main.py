0954a9ccfc001a984f09fc4a879d1488712e86c2
minstrike520 <123291118+minstrike520@users.noreply.github.com>
2023-09-23 01:21:10 +0000

    refactor (src): refactored directory
    Before:
    - core
      - src
        - (game files)
    - tests
    - utilities
    After:
    - src
      - core
        - (game files)
      - tests
      - utilities

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from .tests import instruction_parser_test
+
+# def main():
+    # return instruction_parser_test.main()
+
+if __name__ == "__main__":
+    dir(instruction_parser_test.main())
--- src/tests/instruction_parser_test.py
+++ src/tests/instruction_parser_test.py
@@ -1,8 +1,9 @@
+from ..utilities import instruction_parser as parser
+
 ins_lst = [
     "/main -a 284 -b 284"
 ]
 
 def main():
-    from utilities.src import instruction_parser as parser
     for i in ins_lst:
         print(parser.parse(i))
